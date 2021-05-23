f2cb8c108b63b3413ff868ad78d5b955ecbc9ae9
quoctrung163 <miyucoder@gmail.com>
2021-05-23 16:41:46 +0700

    handle when finish round

---
+++ Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinishLevel : MonoBehaviour
+{
+    public GameObject levelMusic;
+    public AudioSource levelComplete;
+    public GameObject levelTimer;
+
+    void OnTriggerEnter()
+    {
+        // disable levelMusic, levelTimer and enable levelComplete :D
+        levelMusic.SetActive(false);
+        levelTimer.SetActive(false);
+        levelComplete.Play();
+    }
+}
