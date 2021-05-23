138aae13da8bb6906ca2cf127ca516611135bb26
quoctrung163 <miyucoder@gmail.com>
2021-05-23 17:45:31 +0700

    add some handle when finish round

--- Assets/Scripts/FinishLevel.cs
+++ Assets/Scripts/FinishLevel.cs
@@ -9,6 +9,9 @@ public class FinishLevel : MonoBehaviour
     public GameObject levelMusic;
     public AudioSource levelComplete;
     public GameObject levelTimer;
+    public GameObject timeLeft;
+    public GameObject theScore;
+    public GameObject totalScore;
 
     void OnTriggerEnter()
     {
@@ -16,5 +19,16 @@ public class FinishLevel : MonoBehaviour
         levelMusic.SetActive(false);
         levelTimer.SetActive(false);
         levelComplete.Play();
+        StartCoroutine(CalculateScore());
+    }
+
+    IEnumerator CalculateScore()
+    {
+        timeLeft.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        theScore.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        totalScore.SetActive(true);
+        yield return new WaitForSeconds(1f);
     }
 }
--- Assets/Scripts/LevelDeath.cs
+++ Assets/Scripts/LevelDeath.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LevelDeath : MonoBehaviour 
-{    
+public class LevelDeath : MonoBehaviour
+{
+    public GameObject youFell;
+    public GameObject levelAudio;
+    public GameObject fadeOut;
     void OnTriggerEnter()
     {
+        StartCoroutine(YouFellOff());
+    }
+
+    IEnumerator YouFellOff()
+    {
+        youFell.SetActive(true);
+        levelAudio.SetActive(false);
+        yield return new WaitForSeconds(2);
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(0);
     }
 }
